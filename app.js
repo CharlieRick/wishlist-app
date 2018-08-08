41ec0af7e3fb7ed0c1dd2ae7a1ddcc8c6177eefd
CharlieRick <charick@me.com>
2018-08-08 14:19:08 +0100

    add list item styles

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+const wishlist = [
+    {
+      category: 'home',
+      name: 'Deposit for house',
+      price: 24000,
+      link: 'http://www.zoopla.co.uk/',
+      years: 0,
+      months: 0,
+      weeks: 0,
+      days: 0,
+      amountSaved: 7000
+    }
+  ];
+
+  console.log(wishlist);
+
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      wishlists: wishlist
+    },
+    methods: {
+      savedPercentage : function savedPercentage() {
+         var percentage = Math.round(((wishlist[0].amountSaved / wishlist[0].price) * 100));
+         return percentage;
+      },
+
+      catImageLink : function catImageLink() {
+        var catImageLink = 'resources/categories/' + wishlist[0].category + '.svg';
+        return catImageLink;
+      }
     }
   });
 
