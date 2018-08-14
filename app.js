0558c6c65b84eb46a7f0eb5193bcc5979b6fb5e8
CharlieRick <charick@me.com>
2018-08-14 12:00:52 +0100

    add localStorage functionality

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const wishlists = [ 
+var wishlists = [ 
   ];
 
-var display = new Vue({
-    el: '#listContainer',
+var app = new Vue({
+    el: '#app',
     data: {
       wishlists: wishlists,
-      newName: this.name,
-      newPrice: this.price,
-      newAmountSaved: this.amountSaved,
-      newCategory: this.category,
-      newYears: this.years,
-      newMonths: this.months,
-      newWeeks: this.weeks,
-      newLink: this.link
+      editName: this.name,
+      editPrice: this.price,
+      editAmountSaved: this.amountSaved,
+      editCategory: this.category,
+      editYears: this.years,
+      editMonths: this.months,
+      editWeeks: this.weeks,
+      editLink: this.link,
+      newName: '',
+      newPrice: null,
+      newAmountSaved: 0,
+      newCategory: '',
+      newYears: null,
+      newMonths: null,
+      newWeeks: null,
+      newLink: 'http://',
+      error: false,
+      editting: false
     
     },
     methods: {
@@ -34,97 +44,90 @@ var display = new Vue({
       
       
       toggleUpdate: function(index)  {
-        wishlists[index].editting = true;
-        
-        wishlists[index].newName = wishlists[index].name;
-        wishlists[index].newPrice = wishlists[index].price;
-        wishlists[index].newAmountSaved = wishlists[index].amountSaved;
-        wishlists[index].newCategory = wishlists[index].category;
-        wishlists[index].newYears = wishlists[index].years;
-        wishlists[index].newMonths = wishlists[index].months;
-        wishlists[index].newWeeks = wishlists[index].weeks;
-        wishlists[index].newLink = wishlists[index].link;
+        this.wishlists[index].editting = true;
+        this.wishlists[index].editName = this.wishlists[index].name;
+        this.wishlists[index].editPrice = this.wishlists[index].price;
+        this.wishlists[index].editAmountSaved = this.wishlists[index].amountSaved;
+        this.wishlists[index].editCategory = this.wishlists[index].category;
+        this.wishlists[index].editYears = this.wishlists[index].years;
+        this.wishlists[index].editMonths = this.wishlists[index].months;
+        this.wishlists[index].editWeeks = this.wishlists[index].weeks;
+        this.wishlists[index].editLink = this.wishlists[index].link;
+        localStorage.setItem('wishlists', JSON.stringify(this.wishlists));
         },
         updateItem: function(index) {
-          wishlists[index].name = wishlists[index].newName;
-          wishlists[index].price = wishlists[index].newPrice.replace(',', '').replace('£', '');
-          wishlists[index].amountSaved = wishlists[index].newAmountSaved;
-          wishlists[index].category = wishlists[index].newCategory;
-          wishlists[index].years = wishlists[index].newYears;
-          wishlists[index].months = wishlists[index].newMonths;
-          wishlists[index].weeks = wishlists[index].newWeeks;
-          wishlists[index].link = wishlists[index].newLink;
-          wishlists[index].editting = false;
+          this.wishlists[index].name = this.wishlists[index].editName;
+          this.wishlists[index].price = this.wishlists[index].editPrice.replace(',', '').replace('£', '');
+          this.wishlists[index].amountSaved = this.wishlists[index].editAmountSaved;
+          this.wishlists[index].category = this.wishlists[index].editCategory;
+          this.wishlists[index].years = this.wishlists[index].editYears;
+          this.wishlists[index].months = this.wishlists[index].editMonths;
+          this.wishlists[index].weeks = this.wishlists[index].editWeeks;
+          this.wishlists[index].link = this.wishlists[index].editLink;
+          this.wishlists[index].editting = false;
+          localStorage.setItem('wishlists', JSON.stringify(this.wishlists));
         },
 
         scrollToTop: function(){
           $("html, body").animate({ scrollTop: 0 }, "slow");
             return false;
+        },
+
+        addNew: function(){
+          if(!this.newName || !this.newPrice || !this.newCategory) {
+            this.error = true;
+          } else {
+            this.wishlists.push({
+              category: this.newCategory,
+              name: this.newName,
+              price: this.newPrice.replace(',', '').replace('£', ''),
+              link: this.newLink,
+              years: this.newYears,
+              months: this.newMonths,
+              weeks: this.newWeeks,
+              amountSaved: this.newAmountSaved,
+              editting: false
+            })
+            localStorage.setItem('wishlists', JSON.stringify(this.wishlists));
+          }
+          
+        },
+
+        deleteItem: function(index){
+          this.wishlists.splice(index, 1),
+          localStorage.setItem('wishlists', JSON.stringify(this.wishlists));
+        },
+
+        scrollToBottom: function(){
+          if(!this.newName || !this.newPrice || !this.newCategory) {
+            this.error = true;
+          } else {
+            $('html, body').animate({ 
+              scrollTop: $(document).height()}, 
+              400
+            );
+            this.newCategory = '';
+            this.newName = '';
+            this.newPrice = '';
+            this.newLink = 'http://';
+            this.newYears = null;
+            this.newMonths = null;
+            this.newWeeks = null;
+            this.newAmountSaved = 0;
+            this.error = false;
+          }
         }
+      },
+
+      mounted: function mounted() {
+        console.log('App mounted!');
+        if (localStorage.getItem('wishlists')) this.wishlists = JSON.parse(localStorage.getItem('wishlists'));
       }
       
     }
   );
 
-  var input = new Vue({
-    el: '#form',
-    data: {
-      wishlists: wishlists,
-      newName: '',
-      newPrice: null,
-      newAmountSaved: 0,
-      newCategory: '',
-      newYears: null,
-      newMonths: null,
-      newWeeks: null,
-      newLink: 'http://',
-      error: false,
-      editting: false
-
-    },
-    methods: {
-
-      addNew: function(){
-        if(!this.newName || !this.newPrice || !this.newCategory) {
-          this.error = true;
-        } else {
-          this.wishlists.push({
-            category: this.newCategory,
-            name: this.newName,
-            price: this.newPrice.replace(',', '').replace('£', ''),
-            link: this.newLink,
-            years: this.newYears,
-            months: this.newMonths,
-            weeks: this.newWeeks,
-            amountSaved: this.newAmountSaved,
-            editting: false
-          });
-        
-        }
-        
-      }
-      ,
-      scrollToBottom: function(){
-        if(!this.newName || !this.newPrice || !this.newCategory) {
-          this.error = true;
-        } else {
-          $('html, body').animate({ 
-            scrollTop: $(document).height()}, 
-            400
-          );
-          this.newCategory = '';
-          this.newName = '';
-          this.newPrice = '';
-          this.newLink = 'http://';
-          this.newYears = null;
-          this.newMonths = null;
-          this.newWeeks = null;
-          this.newAmountSaved = 0;
-          this.error = false;
-        }
-      }
-    }
-  });
+  //var for wishlists that 
 
 
 // Test Object
